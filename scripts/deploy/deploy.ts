ca287eb61ff6530df07aa06fbef39e1aa2105818
Elwin <jingkangchua@gmail.com>
2023-03-21 10:33:18 +0800

    CD-37

---
+++ scripts/deploy/deploy.ts
@@ -0,0 +1,92 @@
+import { ethers } from "hardhat";
+import hre = require("hardhat");
+import { Signer } from "ethers";
+import { ContractFactory, Contract } from "ethers";
+import { getAddresses } from "../../src/addresses";
+import {
+  IERC20,
+  IUniswapV3Factory,
+  IUniswapV3Pool,
+  ISwapRouter,
+  IGmxPositionRouter,
+  IGmxFastPriceFeed,
+  DerivioA,
+  DerivioAStorage,
+  PositionRouter,
+} from "../../typechain";
+
+
+async function main(): Promise<void> {
+
+  let feeTier = 500
+  let deployer: Signer
+  let otherAccount: Signer
+  let uniswapV3Factory: IUniswapV3Factory
+  let uniswapV3Pool: IUniswapV3Pool
+  let swapRouter: ISwapRouter
+  let gmxPositionRouter: IGmxPositionRouter
+  let gmxFastPriceFeed: IGmxFastPriceFeed
+  let weth: IERC20
+  let usdc: IERC20
+  let derivioA: DerivioA
+  let derivioAStorage: DerivioAStorage
+  let positionRouter: PositionRouter
+
+  let addresses = getAddresses(hre.network.name);
+  [deployer, otherAccount] = await ethers.getSigners();
+
+  uniswapV3Factory = (await ethers.getContractAt("IUniswapV3Factory", addresses.UniswapV3Factory)) as IUniswapV3Factory;
+  uniswapV3Pool = (await ethers.getContractAt("IUniswapV3Pool", await uniswapV3Factory.getPool(addresses.USDC, addresses.WETH, feeTier))) as IUniswapV3Pool;
+  swapRouter = (await ethers.getContractAt("ISwapRouter", addresses.SwapRouter)) as ISwapRouter;
+  gmxPositionRouter = (await ethers.getContractAt("IGmxPositionRouter", addresses.GMXPositionRouter)) as IGmxPositionRouter;
+  gmxFastPriceFeed = (await ethers.getContractAt("IGmxFastPriceFeed", addresses.GMXFastPriceFeed)) as IGmxFastPriceFeed;
+  weth = (await ethers.getContractAt("IERC20", addresses.WETH)) as IERC20;
+  usdc = (await ethers.getContractAt("IERC20", addresses.USDC)) as IERC20;
+
+
+  console.log("Deploying contracts with the account:", deployer.address);
+  console.log("Account balance:", (await deployer.getBalance()).toString());
+
+  const UniHelper: ContractFactory = await ethers.getContractFactory("UniHelper");
+  const uniHelper: Contract = await UniHelper.deploy(addresses.UniswapV3Factory);
+
+  const DerivioAStorage = await ethers.getContractFactory("DerivioAStorage")
+  derivioAStorage = await DerivioAStorage.deploy()
+
+  const DerivioA = await ethers.getContractFactory("DerivioA")
+  derivioA = await DerivioA.deploy(
+    uniHelper.address,
+    addresses.UniswapV3Factory,
+    addresses.SwapRouter,
+    addresses.NonfungiblePositionManager,
+    addresses.GMXPositionRouter,
+    addresses.GMXRouter,
+    addresses.GMXVault,
+    weth.address,
+    usdc.address,
+    false
+  )
+
+  const PositionRouter = await ethers.getContractFactory("PositionRouter")
+  positionRouter = await PositionRouter.deploy(derivioAStorage.address)
+  await positionRouter.addDerivioAPair(
+    uniHelper.address,
+    addresses.UniswapV3Factory,
+    addresses.SwapRouter,
+    addresses.NonfungiblePositionManager,
+    addresses.GMXPositionRouter,
+    addresses.GMXRouter,
+    addresses.GMXVault,
+    weth.address,
+    usdc.address,
+    false
+  )
+  console.log("PositionRouter address:", positionRouter.address);
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
---
+++ scripts/deploy/derivioA-example.ts
@@ -0,0 +1,87 @@
+import { ethers } from "hardhat";
+import hre = require("hardhat");
+import { Signer } from "ethers";
+import { ContractFactory, Contract } from "ethers";
+import { getAddresses } from "../../src/addresses";
+import {
+  IERC20,
+  IUniswapV3Factory,
+  IUniswapV3Pool,
+  ISwapRouter,
+  IGmxPositionRouter,
+  IGmxFastPriceFeed,
+  DerivioA,
+  DerivioAStorage,
+  PositionRouter,
+} from "../../typechain";
+import { fundErc20 } from "../../src/fundErc20";
+
+
+async function main(): Promise<void> {
+
+  let positionRouterAddress = '0x57AC7efDC029559852115fBF8b8a59EAfa70a543'
+  let feeTier = 500
+  let owner: Signer
+  let otherAccount: Signer
+  let uniswapV3Factory: IUniswapV3Factory
+  let uniswapV3Pool: IUniswapV3Pool
+  let swapRouter: ISwapRouter
+  let gmxPositionRouter: IGmxPositionRouter
+  let gmxFastPriceFeed: IGmxFastPriceFeed
+  let weth: IERC20
+  let usdc: IERC20
+  let derivioA: DerivioA
+  let derivioAStorage: DerivioAStorage
+  let positionRouter: PositionRouter
+
+  let addresses = getAddresses(hre.network.name);
+  [owner, otherAccount] = await ethers.getSigners();
+
+  uniswapV3Factory = (await ethers.getContractAt("IUniswapV3Factory", addresses.UniswapV3Factory)) as IUniswapV3Factory;
+  uniswapV3Pool = (await ethers.getContractAt("IUniswapV3Pool", await uniswapV3Factory.getPool(addresses.USDC, addresses.WETH, feeTier))) as IUniswapV3Pool;
+  swapRouter = (await ethers.getContractAt("ISwapRouter", addresses.SwapRouter)) as ISwapRouter;
+  gmxPositionRouter = (await ethers.getContractAt("IGmxPositionRouter", addresses.GMXPositionRouter)) as IGmxPositionRouter;
+  gmxFastPriceFeed = (await ethers.getContractAt("IGmxFastPriceFeed", addresses.GMXFastPriceFeed)) as IGmxFastPriceFeed;
+  weth = (await ethers.getContractAt("IERC20", addresses.WETH)) as IERC20;
+  usdc = (await ethers.getContractAt("IERC20", addresses.USDC)) as IERC20;
+  positionRouter = (await ethers.getContractAt("PositionRouter", positionRouterAddress)) as PositionRouter;
+
+  const slot0 = await uniswapV3Pool.slot0()
+  const tickSpacing = await uniswapV3Pool.tickSpacing()
+
+  const lowerTick = slot0.tick - (slot0.tick % tickSpacing) - 25 * tickSpacing
+  const upperTick = slot0.tick - (slot0.tick % tickSpacing) + 10 * tickSpacing
+  
+  console.log('balanceOf: ' + await usdc.balanceOf(owner.address))
+  // await fundErc20(usdc, addresses.USDCWhale, owner.address, 1000, 6)
+  // console.log('balanceOf: ' + await usdc.balanceOf(owner.address))
+  
+  await weth.approve(positionRouter.address, ethers.constants.MaxUint256)
+  await usdc.approve(positionRouter.address, ethers.constants.MaxUint256)
+  
+  await positionRouter.openDerivioA(
+    {
+      recipient: owner.address,
+      tickLower: lowerTick,
+      tickUpper: upperTick,
+      feeTier: feeTier,
+      amount0Desired: 0,
+      amount1Desired: ethers.utils.parseUnits("1000", 6),
+      shortLeverage: 0,
+      swapMaxSlippage: 0,
+      shortMaxSlippage: 0,
+    },
+    weth.address,
+    usdc.address,
+  )
+
+  // console.log(await positionRouter.getPairId(0, weth.address, usdc.address))
+  console.log(await positionRouter.positionsOf(owner.address))
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error: Error) => {
+    console.error(error);
+    process.exit(1);
+  });
