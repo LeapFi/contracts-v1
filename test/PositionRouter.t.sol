a0f57e386cc6e7e1abfc84c54258c47028b8cedf
Elwin <jingkangchua@gmail.com>
2023-06-26 13:25:17 +0800

    CD-53

--- test/PositionRouter.t.sol
+++ test/PositionRouter.t.sol
@@ -1,13 +1,17 @@
+pragma solidity 0.8.13;
 import {Test} from "forge-std/Test.sol";
+import "forge-std/console.sol";
 
 contract TestPositionRouter is Test {
-    MyContract myContract;
 
     function setUp() public {
-
+        console.log("%s:%s", "foo");
+        console.log('Hello world');
     }
 
     function test1() public {
-        
+    }
+
+    function test2() public {
     }
 }
